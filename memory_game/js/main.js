b6779ef38c6882071989068a671caac026b02e3b
Abdulrahman Ahmed <d7oom@MacBook-Pro.local>
2020-01-27 08:20:59 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 alert('Hello, friends.');
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"	
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMath(){
@@ -8,13 +30,19 @@ function checkForMath(){
   console.log("You found a match!");
 } else {
   console.log("Sorry, try again.");
+
 }
 
 }
 
 function flipCard (cardID){
-	console.log("User flipped "+ cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped "+ cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+
+
+
 
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -24,6 +52,7 @@ if (cardsInPlay.length === 2) {
 		alert ('Sorry, try again');
 	}
 	checkForMath();
+
 }
 
 
